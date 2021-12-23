3c3730079913750e94138da519f8290ee32329d3
khinmyatmon13 <developer.khinmyatmon@gmail.com>
2021-12-23 14:53:43 +0630

    Finished Project

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,77 +13,77 @@ namespace RedisDemoNET5.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 1 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 2 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 3 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 4 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 5 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 6 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 7 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 8 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 9 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using RedisDemoNET5;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 10 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using RedisDemoNET5.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
+#line 11 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\_Imports.razor"
 using Microsoft.Extensions.Caching.Distributed;
 
 #line default
@@ -98,7 +98,7 @@ using Microsoft.Extensions.Caching.Distributed;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "D:\@Projects\RedisTest\RedisDemoNET5\RedisDemoNET5\Pages\Counter.razor"
+#line 9 "D:\@Projects\RedisTest\AzureRedis\RedisDemoNET5\RedisDemoNET5\Pages\Counter.razor"
        
     private int currentCount = 0;
 
--- obj/Debug/net5.0/RedisDemoNET5.AssemblyInfo.cs
+++ obj/Debug/net5.0/RedisDemoNET5.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("fee64964-ba6d-4d4b-ad56-cb088c99db62")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedisDemoNET5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
